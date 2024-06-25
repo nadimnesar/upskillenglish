a6cadf63bc615354558d12477ea5242dc666272c
nadimnesar <nadimnesar.cse@gmail.com>
2024-06-25 13:42:07 +0600

    initial project setup

---
+++ src/main/java/com/brainyfools/upskillenglish/UpskillenglishApplication.java
@@ -0,0 +1,11 @@
+package com.brainyfools.upskillenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpskillenglishApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UpskillenglishApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brainyfools/upskillenglish/UpskillenglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainyfools.upskillenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpskillenglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
